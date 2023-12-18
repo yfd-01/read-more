2b62522907581b0be3584a965468f13833a3227b
yfd-01 <2448543261@qq.com>
2023-12-18 21:15:39 +0800

    inputItem

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import "./index.scss";
+
 const Layout = () => {
-  return <div>Layout</div>
+  const [ inputIconSpin, setInputIconSpin ] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="top">top</div>
+      <div className="content">
+        <div className="inputWrapper">
+          <Input className="inputItem" placeholder="搜索"
+            suffix={
+              <SearchOutlined style={{fontSize: "20px"}} spin={inputIconSpin}
+              onMouseEnter={() => { setInputIconSpin(true) }}
+              onMouseLeave={() => { setInputIconSpin(false) }} />
+            } />
+          </div>
+      </div>
+      <div className="btm">btm</div>
+    </div>
+  )
 }
 
 export default Layout;
